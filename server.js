5382224c271f5adb8e46f187198ba004300d685e
Clement31392 <clementbachimont@orange.fr>
2025-07-22 08:37:16 +0200

    change v2 server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+import e from 'express';
 import app from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`API running at http://localhost:${PORT}`);
 });
+
+export default app;
+export { e as express, PORT };
